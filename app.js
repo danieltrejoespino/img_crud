8d5fcef2a9397c54200c8a3d3faaf8d7c9895cdc
Daniel Trejo <daniel.trejo.espino@gmail.com>
2023-11-04 15:10:54 -0600

    add headers

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const routers= require('./routes/project_route')
 app.use('/api',routers)
 
 app.use((req,res,next)=>{
+
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   const clientIp = req.socket.remoteAddress; 
   console.log(`La IP del cliente que se conectó es: ${clientIp}`);
   next();
